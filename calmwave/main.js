069dacdceeaa7b6490df19631a3dd997e28612fd
Ro Rudra <rr1960@nyu.edu>
2017-04-05 13:11:10 -0400

    cleaning up code and style changes

--- calmwave/main.js
+++ calmwave/main.js
@@ -1,12 +1,12 @@
 var $document = $(document)
 
 $document.scroll(function() {
-  if ($document.scrollTop() >= 150) {
+  if ($document.scrollTop() >= 130) {
       $('span').addClass('half');
     } else {
       $('span').removeClass('half');
     }
-  if ($document.scrollTop() >= 350) {
+  if ($document.scrollTop() >= 200) {
       $('span').addClass('whole');
       $('span').removeClass('half');
     } else {
